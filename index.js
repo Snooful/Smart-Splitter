0974b3a8611c22ed3d77d2f6bc0aef2bea9214b6
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-07 15:55:02 -0500

    Remove logging

--- index.js
+++ index.js
@@ -33,17 +33,14 @@ function split(text = "", count = Infinity, splitter = " ", quotes = defQuotes) 
 	const endQuotes = quotes.map(quote => quote.end);
 
 	let inQuote = false;
-	console.log("starting with text: " + text)
 
 	const chars = text.split("");
 	out.push("");
 	chars.forEach((char, index) => {
 		if (startQuotes.includes(char) && !inQuote) {
-			console.log(`now in quote (${char}) at index ${index}`)
 			inQuote = startQuotes.indexOf(char);
 		} else if (inQuote && endQuotes[inQuote] === char) {
 			if (index > 0 && chars[index - 1] !== "\\") {
-				console.log(`exited quote (${char}) at index ${index}`)
 				inQuote = false;
 			}
 		} else if (char === splitter && out.length < count && !inQuote) {
