1a059b12280305860c1d7b74330ba10d345c0178
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-07 12:00:06 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,56 @@
 const assert = require("chai").assert;
-const { split } = require(".");
+const smartSplitter = require(".");
 
-describe("normal strings", () => {
-	const text = "smart or dumb splitting";
+const text = "Hello, world! This is an example.";
+
+describe("smartSplitter.split", () => {
+	it("is a function", () => {
+		assert.isFunction(smartSplitter.split);
+	});
+	
+	it("returns an array of strings", () => {
+		split(text).forEach(item => {
+			assert.isString(item);
+		});
+	});
+	
 	it("has as many items as dumb split by space", () => {
 		assert.equal(split(text).length, text.split(" ").length);
 	});
-});
 
-describe("count", () => {
 	it("doesn't split if count is 0", () => {
-		assert.deepEqual(split("hello world", 0), [
-			"hello world",
+		assert.deepEqual(split(text, 0), [
+			text,
+		]);
+	});
+	
+	it("doesn't split if whole string is quoted", () => {
+		assert.deepEqual(split("'" + text "'"), [
+			text,
 		]);
 	});
 });
-
-describe("splitting with quotes", () => {
-	it("doesn't split if whole string is quoted", () => {
-		assert.deepEqual(split("'hello world'"), [
-			"hello world",
-		]);
+	 
+describe("smartSplitter.quotes", () => {
+	it("is an array", () => {
+		asset.isArray(smartSplitter.quotes);
+	});
+	
+	it("each item is an object", () => {
+		smartSplitter.quotes.forEach(quote => {
+			assert.isObject(quote);
+		});
+	});
+	
+	it("each quote object has a 'start' string", () => {
+		smartSplitter.quotes.forEach(quote => {
+			assert.isString(quote.start);
+		});
+	});
+	
+	it("each quote object has an 'end' string", () => {
+		smartSplitter.quotes.forEach(quote => {
+			assert.isString(quote.end);
+		});
 	});
 });
