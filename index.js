3338417fff0ede68f42a7f82e916b464df85e757
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-12 21:37:14 -0500

    Fix quotes in middle of word

--- index.js
+++ index.js
@@ -38,6 +38,15 @@ const defQuotes = [
 ];
 
 /**
+ * Checks if every value in an array is truthy.
+ * @param {boolean[]} conditions The conditions that should be fulfilled.
+ * @returns {boolean}
+ */
+function all(conditions = []) {
+	return conditions.every(value => value);
+}
+
+/**
  * Splits a string.
  * @param {string} text The text to split.
  * @param {number} count The number of elements to split the text into.
@@ -56,13 +65,24 @@ function split(text = "", count = Infinity, splitter = " ", quotes = defQuotes) 
 	const chars = text.split("");
 	out.push("");
 	chars.forEach((char, index) => {
-		if (startQuotes.includes(char) && !inQuote) {
+		if (all([
+			!inQuote, // Don't start quote inside another
+			startQuotes.includes(char), // Must be a starting quote
+			out[out.length - 1].length === 0, // At the start of a new split
+		])) {
 			inQuote = startQuotes.indexOf(char);
-		} else if (inQuote && endQuotes[inQuote] === char) {
-			if (index > 0 && chars[index - 1] !== "\\") {
-				inQuote = false;
-			}
-		} else if (char === splitter && out.length < count && !inQuote) {
+		} else if (all([
+			inQuote, // Don't end quote when not inside one
+			endQuotes[inQuote] === char, // Quote must match the start
+			index > 0, // Making sure we don't get an OoB character
+			chars[index - 1] !== "\\", // Is not escaped
+			[" ", undefined].includes(chars[index + 1]) // Next character is a space or end of string (end of split)
+		])) {
+			inQuote = false;
+		} else if (all([
+			char === splitter,
+			out.length < count && !inQuote,
+		])) {
 			out.push("");
 		} else {
 			out[out.length - 1] += char;
